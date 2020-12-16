35661f2ae1b1e94c323ad6e01892d987962dba8e
Plutaniano <lucasrodriguessilva@hotmail.com>
2020-12-16 14:52:58 -0300

    day15

--- 15/solution.py
+++ 15/solution.py
@@ -7,7 +7,8 @@ class Game:
     def __init__(self, numbers: List[int]):
         self.spoken_pos = {}
         self.numbers = numbers
-        for i, n in enumerate(numbers, start=1):
+        self.n_len = len(numbers) - 1
+        for i, n in enumerate(numbers):
             try:
                 self.spoken_pos[n].append(i)
             except KeyError:
@@ -15,16 +16,24 @@ class Game:
     
     def speak(self) -> None:
         last = self.numbers[-1]
-        if last not in self.numbers:
-            self.numbers.append(0)
-            self.spoken_pos[0].append(len(numbers))
+        if len(self.spoken_pos[last]) <= 1:
+            number = 0
         else:
-            number = self.spoken_pos[last][0] - self.spoken_pos[last][1]
-            self.numbers.append(number)
-            self.spoken_pos[number].append(len(numbers))
+            number = self.spoken_pos[last][-1] - self.spoken_pos[last][-2]
+        self.numbers.append(number)
+        try:
+            self.spoken_pos[number].append(len(self.numbers) - 1)
+        except:
+                self.spoken_pos[number] = [len(self.numbers) -1]
+
+g = Game([0,6,1,7,2,19,20])
+
+for i in range(30000000):
+    g.speak()
 
 
-
+print(f'Part 1: {g.numbers[2020-1]}')
+print(f'Part 2: {g.numbers[30000000-1]}')
         
 
 
