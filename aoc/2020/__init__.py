fd534d5c5febc009cb97b2ecaacaf0716680abb3
Lucas R <github@lucas.rs>
2022-09-18 01:33:36 -0300

    update __init__

--- aoc/2020/__init__.py
+++ aoc/2020/__init__.py
@@ -1,5 +1,25 @@
-for day in range(1, 26):
-    try:
-        __import__(f"aoc.2020.day{day:02}", fromlist=["Solution"])
-    except ImportError:
-        pass
+from .day01 import Day01
+from .day02 import Day02
+from .day03 import Day03
+from .day04 import Day04
+from .day05 import Day05
+from .day06 import Day06
+from .day07 import Day07
+from .day08 import Day08
+# from .day09 import Day09
+# from .day10 import Day10
+# from .day11 import Day11
+from .day12 import Day12
+from .day13 import Day13
+# from .day14 import Day14
+# from .day15 import Day15
+# from .day16 import Day16
+# from .day17 import Day17
+# from .day18 import Day18
+# from .day19 import Day19
+# from .day20 import Day20
+# from .day21 import Day21
+# from .day22 import Day22
+# from .day23 import Day23
+# from .day24 import Day24
+# from .day25 import Day25
